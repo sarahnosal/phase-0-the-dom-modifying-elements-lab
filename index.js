89a1d4ead32dd5712f5213a59d8b7500fc374af3
Sarah Nosal <nosalsm@gmail.com>
2021-11-12 09:40:44 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.querySelector("main");
+main.remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+newHeader.innerHTML = "<h1>Sarah is the champion</h1>";
